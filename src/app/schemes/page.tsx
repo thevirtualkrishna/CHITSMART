85c5314eb1f94da07430cdb4af6db280a522d8e6
Saikrishna Jella <contactkrishnaworks@gmail.com>
2025-08-06 23:36:00 +0530

    Update page.tsx

--- src/app/schemes/page.tsx
+++ src/app/schemes/page.tsx
@@ -1,3 +1,4 @@
+
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Users, Wallet, Calendar } from 'lucide-react';
 import Link from "next/link";
